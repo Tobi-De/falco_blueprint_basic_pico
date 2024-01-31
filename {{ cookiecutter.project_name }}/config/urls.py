579356df4d87af3bf82b13cc9831707062734649
Tobi DEGNON <tobidegnon@proton.me>
2024-01-31 09:39:43 +0100

    start reworking base templates

--- {{ cookiecutter.project_name }}/config/urls.py
+++ {{ cookiecutter.project_name }}/config/urls.py
@@ -4,9 +4,11 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views import defaults as default_views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
 ]
@@ -15,5 +17,21 @@ if settings.DEBUG:
     urlpatterns += [
         path("__reload__/", include("django_browser_reload.urls")),
         path("__debug__/", include("debug_toolbar.urls")),
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     ]
