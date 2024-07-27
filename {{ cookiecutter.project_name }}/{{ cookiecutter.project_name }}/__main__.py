f99524b16d7e8cafce9ce16dfb9ee67c9234d54c
Tobi DEGNON <tobidegnon@proton.me>
2024-07-27 07:32:07 +0100

    fix gunicorn config
    The module gunicorn.py was in conflict with the actual package, and
    since i'm already running it from a python script, and extra config file
    is unecessary

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import sys
 
@@ -5,7 +6,9 @@ from gunicorn.app import wsgiapp
 
 
 def main() -> None:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings"
+    )
 
     run_func = None
     if len(sys.argv) > 1:
@@ -31,11 +34,31 @@ def run_manage(argv: list) -> None:
     execute_from_command_line(argv[1:])
 
 
-def run_gunicorn(argv: list) -> None:
-    """Run the web server."""
-    argv.append("{{ cookiecutter.project_name }}.wsgi:application")
-    argv.append("--config")
-    argv.append("gunicorn.conf.py")
+def run_gunicorn(argv) -> None:
+    """
+    Run gunicorn the wsgi server.
+    https://docs.gunicorn.org/en/stable/settings.html
+    https://adamj.eu/tech/2021/12/29/set-up-a-gunicorn-configuration-file-and-test-it/
+    """
+
+    workers = multiprocessing.cpu_count() * 2 + 1
+    gunicorn_args = [
+        "{{ cookiecutter.project_name }}.wsgi:application",
+        "--bind",
+        "127.0.0.1:8000",
+        # "unix:/run/{{ cookiecutter.project_name }}.gunicorn.sock", # uncomment this line and comment the line above to use a socket file
+        "--max-requests",
+        "1000",
+        "--max-requests-jitter",
+        "50",
+        "--workers",
+        str(workers),
+        "--access-logfile",
+        "-",
+        "--error-logfile",
+        "-",
+    ]
+    argv.extend(gunicorn_args)
     wsgiapp.run()
 
 
