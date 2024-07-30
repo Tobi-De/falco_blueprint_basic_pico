45b3e14efded9349a4322a2413509b95ef4d278d
Tobi DEGNON <tobidegnon@proton.me>
2024-07-30 06:36:36 +0100

    improve s6 setup, rely more on app entry point (#72)

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/__main__.py
@@ -1,14 +1,11 @@
-import multiprocessing
-import os
-import sys
-
-from gunicorn.app import wsgiapp
-
-
 def main() -> None:
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings"
-    )
+    from pathlib import Path
+    import os
+    import sys
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings")
+    current_path = Path(__file__).parent.parent.resolve()
+    sys.path.append(str(current_path))
 
     run_func = None
     if len(sys.argv) > 1:
@@ -20,26 +17,27 @@ def main() -> None:
         run_gunicorn(sys.argv)
 
 
-def run_qcluster(argv: list) -> None:
-    """Run Django-q cluster."""
+def run_setup(_):
+    """Run some project setup tasks"""
     from django.core.management import execute_from_command_line
+    from django.core.management.base import CommandError
+    from contextlib import suppress
 
-    execute_from_command_line(argv[2:])
+    execute_from_command_line(["manage", "migrate"])
+    execute_from_command_line(["manage", "setup_periodic_tasks"])
+
+    with suppress(CommandError):
+        execute_from_command_line(["manage", "createsuperuser", "--noinput", "--traceback"])
 
 
-def run_manage(argv: list) -> None:
-    """Run Django's manage command."""
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(argv[1:])
-
-
-def run_gunicorn(argv) -> None:
+def run_gunicorn(argv: list) -> None:
     """
     Run gunicorn the wsgi server.
     https://docs.gunicorn.org/en/stable/settings.html
     https://adamj.eu/tech/2021/12/29/set-up-a-gunicorn-configuration-file-and-test-it/
     """
+    import multiprocessing
+    from gunicorn.app import wsgiapp
 
     workers = multiprocessing.cpu_count() * 2 + 1
     gunicorn_args = [
@@ -62,7 +60,21 @@ def run_gunicorn(argv) -> None:
     wsgiapp.run()
 
 
-COMMANDS = {"qcluster": run_qcluster, "manage": run_manage}
+def run_qcluster(argv: list) -> None:
+    """Run Django-q cluster."""
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(argv[2:])
+
+
+def run_manage(argv: list) -> None:
+    """Run Django's manage command."""
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(argv[1:])
+
+
+COMMANDS = {"qcluster": run_qcluster, "manage": run_manage, "setup": run_setup}
 
 
 if __name__ == "__main__":
